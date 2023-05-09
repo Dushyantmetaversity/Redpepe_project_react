1745582df014bd3bebc48915145fdf94a4d22d62
Dushyantmetaversity <dushyant@metaversitytechnologies.com>
2023-05-09 22:00:08 +0530

    changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,29 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Logo from "./img/fdsd.png";
 import { motion, useMotionValue, useTransform } from "framer-motion";
-
+// import { Turn as Hamburger } from 'hamburger-react'
+import { Turn as Hamburger } from 'hamburger-react';
 const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
   const x = useMotionValue(0);
   const opacity = useTransform(x, [-100, 0, 100], [0, 1, 0]);
   return (
-    <header className="headercsss">
-      <div>
-        <img className="logogogog" src={Logo} alt="logo" />
-      </div>
-      <div className="listttt">
-        <ul>
-          <li>Home</li>
-          <li>about</li>
-          <li>how to buy</li>
-          <li>tokenomic</li>
-          <li>roadap</li>
-        </ul>
-      </div>
-      <div className="buttonnn">
-        <button className="buybutton"> buy now</button>
-      </div>
-    </header>
+    <div className="overlay">
+      {isOpen ? <header className="headercsss">
+        <div className="_burgermenu">
+          <img className="logogogog" src={Logo} alt="logo" />
+
+          <div>
+            <Hamburger toggled={isOpen} toggle={setOpen} color="#fff" />
+          </div>
+        </div>
+        <div className="listttt">
+          <ul>
+            <li>Home</li>
+            <li>about</li>
+            <li>how to buy</li>
+            <li>tokenomic</li>
+            <li>roadap</li>
+          </ul>
+        </div>
+        <div className="buttonnn">
+          <button className="buybutton"> buy now</button>
+        </div>
+      </header>
+
+        : <header className="headercsss">
+          <div className="_burgermenu">
+            <img className="logogogog" src={Logo} alt="logo" />
+
+            <div>
+              <Hamburger toggled={isOpen} toggle={setOpen} color="#fff" />
+            </div>
+          </div>
+          <div className="listttt">
+            <ul>
+              <li>Home</li>
+              <li>about</li>
+              <li>how to buy</li>
+              <li>tokenomic</li>
+              <li>roadap</li>
+            </ul>
+          </div>
+          <div className="buttonnn">
+            <button className="buybutton"> buy now</button>
+          </div>
+        </header>
+
+      }
+    </div>
   );
 };
 
